086f9c0b5a881adbe43ed1045720536ab744a8fc
julienbrg <julien@strat.cc>
2024-11-05 11:50:27 +0100

    clean up

--- contracts/IStakeProxy.sol
+++ contracts/IStakeProxy.sol
@@ -2,12 +2,6 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-// import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-
 /** Custom Errors */
 error NotTokenOwner(uint256 tokenId, address caller);
 error InvalidAddress();
